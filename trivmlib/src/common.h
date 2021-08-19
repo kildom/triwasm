348b50ebc7da59eb7101266ab18ff713b8bbc11c
kildom <kildom@users.noreply.github.com>
2021-08-19 22:08:16 +0200

    Resolved export of triVM assembly and license annotations in trivmlib

--- trivmlib/src/common.h
+++ trivmlib/src/common.h
@@ -4,16 +4,42 @@
 #include <stdint.h>
 
 #define EXPORT(name) __attribute__((used)) __attribute__((export_name(#name)))
-#define IMPORT(module, name) __attribute__((used)) __attribute__((import_module(module))) __attribute__((import_name(#name)))
+#define IMPORT(module, name) __attribute__((used)) __attribute__((import_module(#module))) __attribute__((import_name(#name)))
+#define IMPORT_BUILTIN(name) __attribute__((used)) __attribute__((import_module("__trivm_magic_function__"))) __attribute__((import_name("builtin:" #name)))
 
 #define TRIVM_ASSEMBLY(code) \
 __attribute__((used)) \
-__attribute__((import_module("__trivm_assembly_function__"))) \
-__attribute__((import_name(code)))
+__attribute__((import_module("__trivm_magic_function__"))) \
+__attribute__((import_name("assembly::" code)))
 
 #define TRIVM_INLINE_ASSEMBLY(code) \
 __attribute__((used)) \
-__attribute__((import_module("__trivm_inline_assembly_function__"))) \
-__attribute__((import_name(code)))
+__attribute__((import_module("__trivm_magic_function__"))) \
+__attribute__((import_name("assembly:inline:" code)))
+
+#define TRIVM_EXPORT_ASSEMBLY(name, code, ret_type, params) \
+__attribute__((used)) \
+__attribute__((export_name("__trivm_magic_function__:assembly:export=" #name ":" code))) \
+ret_type __trivmlib_tmp_assembly2_##name params { __builtin_unreachable(); }
+
+/* TODO: In case TRIVM_EXPORT_ASSEMBLY have to be called locally
+__attribute__((used)) \
+__attribute__((import_module("__trivm_magic_function__"))) \
+__attribute__((import_name("alias:export:" #exp_name))) \
+ret_type c_name params;
+*/
+
+#define TRIVM_EXPORT_INLINE_ASSEMBLY(name, code, ret_type, params) \
+__attribute__((used)) \
+__attribute__((export_name("__trivm_magic_function__:assembly:inline,export=" #name ":" code))) \
+ret_type __trivmlib_tmp_assembly2_##name params { __builtin_unreachable(); }
+
+#define DEFINE_ANNOTATION(name, text) \
+__attribute__((used)) \
+__attribute__((import_module("__trivm_magic_function__"))) \
+__attribute__((import_name("annotation:" text))) \
+void __trivm_annotation__##name(void);
+
+#define USE_ANNOTATION(name) __trivm_annotation__##name()
 
 #endif
--- trivmlib/src/f32.c
+++ trivmlib/src/f32.c
@@ -3,54 +3,94 @@
 
 #include "softfloat.h"
 
+DEFINE_ANNOTATION(license_f32, "license:Berkeley-SoftFloat");
+
 #define EXP_BINOP32(T, name) \
 EXPORT(trivm_##name) \
 T trivm_##name( float32_t a, float32_t b ) \
 { \
+    USE_ANNOTATION(license_f32); \
     return name(a, b); \
 } \
 
-EXP_BINOP32(bool, f32_eq);
-EXP_BINOP32(bool, f32_lt);
-EXP_BINOP32(bool, f32_le);
 EXP_BINOP32(float32_t, f32_add);
 EXP_BINOP32(float32_t, f32_sub);
 EXP_BINOP32(float32_t, f32_mul);
 EXP_BINOP32(float32_t, f32_div);
 
-EXPORT(trivm_f32_gt)
-bool trivm_f32_gt( float32_t a, float32_t b )
+DEFINE_ANNOTATION(trivm_f32_le_name, "triasm_name:__trivmlib_f32_le");
+
+EXPORT(trivm_f32_le)
+bool trivm_f32_le( float32_t a, float32_t b )
 {
-    return trivm_f32_lt(b, a);
+    USE_ANNOTATION(trivm_f32_le_name);
+    USE_ANNOTATION(license_f32);
+    return f32_le(a, b);
 }
 
-EXPORT(trivm_f32_ge)
-bool trivm_f32_ge( float32_t a, float32_t b )
+DEFINE_ANNOTATION(trivm_f32_lt_name, "triasm_name:__trivmlib_f32_lt");
+
+EXPORT(trivm_f32_lt)
+bool trivm_f32_lt( float32_t a, float32_t b )
 {
-    return trivm_f32_le(b, a);
+    USE_ANNOTATION(trivm_f32_lt_name);
+    USE_ANNOTATION(license_f32);
+    return f32_lt(a, b);
 }
 
-EXPORT(trivm_f32_ne)
-bool trivm_f32_ne( float32_t a, float32_t b )
+DEFINE_ANNOTATION(trivm_f32_eq_name, "triasm_name:__trivmlib_f32_eq");
+
+EXPORT(trivm_f32_eq)
+bool trivm_f32_eq( float32_t a, float32_t b )
 {
-    return !trivm_f32_eq(b, a);
+    USE_ANNOTATION(trivm_f32_eq_name);
+    USE_ANNOTATION(license_f32);
+    return f32_eq(a, b);
 }
+
+TRIVM_EXPORT_ASSEMBLY(
+    trivm_f32_gt,
+    "READ [SP] + 2\n"
+    "READ [SP] + 2\n"
+    "WRITE [SP] + 3\n"
+    "WRITE [SP] + 1\n"
+    "BR __trivmlib_f32_lt\n",
+    bool, ( float32_t a, float32_t b ));
+
+TRIVM_EXPORT_ASSEMBLY(
+    trivm_f32_ge,
+    "READ [SP] + 2\n"
+    "READ [SP] + 2\n"
+    "WRITE [SP] + 3\n"
+    "WRITE [SP] + 1\n"
+    "BR __trivmlib_f32_le\n",
+    bool, ( float32_t a, float32_t b ));
+
+TRIVM_EXPORT_ASSEMBLY(
+    trivm_f32_ne,
+    "CALL __trivmlib_f32_eq\n"
+    "NOT\n"
+    "RET\n",
+    bool, ( float32_t a, float32_t b ));
 
 EXPORT(trivm_f32_ceil)
 float32_t trivm_f32_ceil( float32_t a )
 {
+    USE_ANNOTATION(license_f32);
     return f32_roundToInt(a, softfloat_round_max, false);
 }
 
 EXPORT(trivm_f32_floor)
 float32_t trivm_f32_floor( float32_t a )
 {
+    USE_ANNOTATION(license_f32);
     return f32_roundToInt(a, softfloat_round_min, false);
 }
 
 EXPORT(trivm_f32_trunc)
 float32_t trivm_f32_trunc( float32_t a )
 {
+    USE_ANNOTATION(license_f32);
     return f32_roundToInt(a, softfloat_round_minMag, false);
 }
 
@@ -58,35 +98,41 @@ float32_t trivm_f32_trunc( float32_t a )
 EXPORT(trivm_f32_nearest)
 float32_t trivm_f32_nearest( float32_t a )
 {
+    USE_ANNOTATION(license_f32);
     return f32_roundToInt(a, softfloat_round_near_even, false);
 }
 
 EXPORT(trivm_f32_convert_i32_s)
 float32_t trivm_f32_convert_i32_s(int32_t a)
 {
+    USE_ANNOTATION(license_f32);
     return i32_to_f32(a);
 }
 
 EXPORT(trivm_f32_convert_i32_u)
 float32_t trivm_f32_convert_i32_u(int32_t a)
 {
+    USE_ANNOTATION(license_f32);
     return ui32_to_f32(a);
 }
 
 EXPORT(trivm_f32_convert_i64_s)
 float32_t trivm_f32_convert_i64_s(int64_t a)
 {
+    USE_ANNOTATION(license_f32);
     return i64_to_f32(a);
 }
 
 EXPORT(trivm_f32_convert_i64_u)
 float32_t trivm_f32_convert_i64_u(int64_t a)
 {
+    USE_ANNOTATION(license_f32);
     return ui64_to_f32(a);
 }
 
 EXPORT(trivm_f32_demote_f64)
 float32_t trivm_f32_demote_f64(float64_t a)
 {
+    USE_ANNOTATION(license_f32);
     return f64_to_f32(a);
 }
