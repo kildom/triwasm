1a01a8919c3c002c63eacdd6ebeaa91a5defbbcb
kildom <kildom@users.noreply.github.com>
2022-08-31 15:35:48 +0200

    Update SystemRegisterBegin.js

--- tools/utils/build/SystemRegisterBegin.js
+++ tools/utils/build/SystemRegisterBegin.js
@@ -12,50 +12,58 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-const System = (function() {
-	const System = {};
-	const modules = {};
-	function execModule(module) {
-		if (module.__executed__) {
-			return;
-		}
-		module.__executed__ = true;
-		for (let dep of module.__deps__) {
-			execModule(modules[dep]);
-		}
-		module.__execute__();
-		delete module.__execute__;
-	}
-	System.register = function(name, deps, declare) {
-		let module = {
-			__deps__: deps,
-			__declare__: declare,
-			__executed__: false,
-		};
-		module.__exports__ = function(name, value) {
-			module[name] = value;
-		};
-		modules[name] = module;
-	};
-	System.__execute__ = function() {
-		for (let name in modules) {
-			let module = modules[name];
-			let {setters, execute} = module.__declare__(module.__exports__);
-			delete module.__declare__;
-			module.__setters__ = setters;
-			module.__execute__ = execute;
-		}
-		for (let name in modules) {
-			let module = modules[name];
-			for (let i = 0; i < module.__deps__.length; i++) {
-				let dep = module.__deps__[i];
-				module.__setters__[i](modules[dep]);
-			}
-			delete module.__setters__;
-		}
-		for (let module of Object.values(modules).reverse()) {
-			execModule(module);
-		}
-	}
-	return System;
+
+const System = (function () {
+    const System = {};
+    const modules = {};
+    function execModule(module) {
+        if (module.__executed__) {
+            return;
+        }
+        module.__executed__ = true;
+        for (let dep of module.__deps__) {
+            if (dep in modules) {
+                execModule(modules[dep]);
+            }
+        }
+        module.__execute__();
+        delete module.__execute__;
+    }
+    System.register = function (name, deps, declare) {
+        let module = {
+            __deps__: deps,
+            __declare__: declare,
+            __executed__: false,
+        };
+        module.__exports__ = function (name, value) {
+            module[name] = value;
+        };
+        modules[name] = module;
+    };
+    System.__execute__ = function () {
+        for (let name in modules) {
+            let module = modules[name];
+            let { setters, execute } = module.__declare__(module.__exports__);
+            delete module.__declare__;
+            module.__setters__ = setters;
+            module.__execute__ = execute;
+        }
+        for (let name in modules) {
+            let module = modules[name];
+            for (let i = 0; i < module.__deps__.length; i++) {
+                let dep = module.__deps__[i];
+                if (dep in modules) {
+                    module.__setters__[i](modules[dep]);
+                } else {
+                    module.__setters__[i](require(dep));
+                }
+            }
+            delete module.__setters__;
+        }
+        console.log(modules);
+        for (let module of Object.values(modules).reverse()) {
+            execModule(module);
+        }
+    };
+    return System;
 })();
