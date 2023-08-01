cd93350c53b71ba8d03e0bdff2f56272969787e6
kildom <kildom@users.noreply.github.com>
2023-08-01 16:06:33 +0200

    Update args.ts

--- tools/wasm/args.ts
+++ tools/wasm/args.ts
@@ -8,18 +8,25 @@ export enum OptLevel {
     FULL = 2,
 }
 
+interface WasmArgsMerge {
+    name: string;
+    file: Path;
+    globalExports: boolean;
+}
+
 export interface WasmArgs {
     input: Path;
     confg: Path;
-    output?: Path;
+    output: Path;
     optimize: OptLevel;
+    vmStackSize: number | 'shared';
+    globalBase: number;
+    guestStackFirst: boolean;
+    guestStackGuard: boolean;
+    guestStackSize?: number;
     assembly: boolean;
+    merge: WasmArgsMerge[];
     debugDump: boolean;
-    merge: {
-        name: string;
-        file: Path;
-        globalExports: boolean;
-    }[];
 }
 
 export wasmUsage = ```
@@ -40,23 +47,44 @@ Compile WebAssembly module file to triVM binary file.
     but with different extension.
 
 -O
---optimize:OptLevel <level>
+--optimize:OptLevel <level> = 2
     Optimize the output. By default, maximum optimization is enabled.
     Optimization levels: 0 - no optimization, 1 - basic optimization, 2 and more - maximum
-    optimization, s and z are aliases for 2. The optimization strategy is always
-    focused on size.
+    optimization, "s", "z" and "fast" are aliases for 2, "g" is alias for 1. The optimization
+    strategy is always focused on size.
+
+--vm-stack-size:VmStackSize <size> = 1024
+    Set size of the virtual machine stack. Default is 1024. Special value 'shared' puts the
+    virtual machine stack at the guest stack.
+
+--global-base:size <address> = 0
+    Address at which module starts storing data. Everything before that is unused by the
+    module and can be used by the compiler and virtual machine. Required for memory size
+    smaller than 64K. By default, it is 0.
+
+--guest-stack-first
+    Gest have its stack first, before '--global-base' address.
+
+--guest-stack-guard
+    Enable gest stack guard. Module must contain special function detecting the stack pointer
+    global variable and virtual machine must have guest stack overflow or underflow
+    fault enabled.
+
+--guest-stack-size:size <size>
+    Size of the guest stack. It is used only if '--guest-stack-first' or '--guest-stack-guard'
+    options are enabled.
 
 -S
 --assembly
     Compile to assembly text file.
 
---merge:merge[0-] <name=file>
+--merge:Merge[0-] <name=file>
     Merge additinal WebAssembly module into the output. The module can contain only functions.
     If name is prefixed with '!', exports from module will be available as global (guest-host)
     exports. Otherwise, the module exports can be used only internally by other modules.
 
 --debug-dump
-    Dump various internal files alongside the output file. Use it to debugg the compiler.
+    Dump various internal files alongside the output file. Use it to debug the compiler.
 
 --version:!ver
     Display version information.
@@ -66,4 +94,39 @@ Compile WebAssembly module file to triVM binary file.
 
 ```;
 
-export const args: WasmArgs = parse<WasmArgs>(wasmUsage);
+const filters = {
+    Merge: (arg: any, option: Option, parser: ArgsParser): WasmArgsMerge => {
+        let [name, file] = (arg as string).split('=', 2);
+        let globalExports = false;
+        if (name.startsWith('!')) {
+            name = name.substring(1);
+            globalExports = true;
+        }
+        return { name, file: argparse.stdFilters.Path(file), globalExports };
+    }
+    VmStackSize: (arg: any, option: Option, parser: ArgsParser): number | 'shared' => {
+        if (arg === 'shared') {
+            return arg;
+        } else {
+            return argparse.stdFilters.size(arg, option, parser);
+        }
+    }
+    OptLevel: (arg: any, option: Option, parser: ArgsParser): OptLevel => {
+        if (arg === 's' || arg === 'z' || arg === 'fast') {
+            return OptLevel.FULL;
+        } else if (arg === 'g') {
+            return OptLevel.BASIC;
+        } else {
+            let value = argparse.stdFilters.int(arg, option, parser);
+            return Math.max(0, Math.min(2, value)) as OptLevel;
+        }
+    }
+};
+
+function postProcess(results: any, name: string, parser: ArgsParser) {
+    if (results.output === undefined) {
+        results.output = results.input.withExt(results.assembly ? '.triasm' : '.trivm');
+    }
+};
+
+export const args: WasmArgs = parse<WasmArgs>(wasmUsage, filters, postProcess);
