75150aca7afaec8c083bf3b9b30b9d497deccd02
kildom <kildom@users.noreply.github.com>
2023-08-02 16:48:30 +0200

    Update args.ts

--- tools/wasm/args.ts
+++ tools/wasm/args.ts
@@ -30,15 +30,15 @@ export interface WasmArgs {
 }
 
 export wasmUsage = ```
-triwasm [options] -c <trivm-config> <input>
+ triwasm [options] -c <trivm-config> <input>
 
-Compile WebAssembly module file to triVM binary file.
+ Compile WebAssembly module file to triVM binary file.
 
-:Path <input>
+:Path[1] <input>
     Compile WebAssembly module from <input>.
 
 -c
---config:Path <file>
+--config:Path[1] <file>
     Get triVM configuration from <file>.
 
 -o
@@ -49,49 +49,50 @@ Compile WebAssembly module file to triVM binary file.
 -O
 --optimize:OptLevel <level> = 2
     Optimize the output. By default, maximum optimization is enabled.
-    Optimization levels: 0 - no optimization, 1 - basic optimization, 2 and more - maximum
-    optimization, "s", "z" and "fast" are aliases for 2, "g" is alias for 1. The optimization
-    strategy is always focused on size.
+    Optimization levels: 0 - no optimization, 1 - basic optimization,
+    2 and more - maximum optimization, "s", "z" and "fast" are aliases for 2,
+    "g" is alias for 1. The optimization strategy is always focused on size.
 
---vm-stack-size:VmStackSize <size> = 1024
-    Set size of the virtual machine stack. Default is 1024. Special value 'shared' puts the
-    virtual machine stack at the guest stack.
+--vm-stack-size:VmStackSize <size>|shared = 1024
+    Set size of the virtual machine stack. Default is 1024. Special value
+    "shared" puts the virtual machine stack at the guest stack.
 
 --global-base:size <address> = 0
-    Address at which module starts storing data. Everything before that is unused by the
-    module and can be used by the compiler and virtual machine. Required for memory size
-    smaller than 64K. By default, it is 0.
+    Address at which module starts storing data. Everything before that is
+    unused by the module and can be used by the compiler and virtual machine.
+    Required for memory size smaller than 64K. By default, it is 0.
 
 --guest-stack-first
     Gest have its stack first, before '--global-base' address.
 
 --guest-stack-guard
-    Enable gest stack guard. Module must contain special function detecting the stack pointer
-    global variable and virtual machine must have guest stack overflow or underflow
-    fault enabled.
+    Enable gest stack guard. Module must contain special function detecting the
+    stack pointer global variable and virtual machine must have guest stack
+    overflow or underflow fault enabled.
 
 --guest-stack-size:size <size>
-    Size of the guest stack. It is used only if '--guest-stack-first' or '--guest-stack-guard'
-    options are enabled.
+    Size of the guest stack. It is used only if "--guest-stack-first" or
+    "--guest-stack-guard" options are enabled.
 
 -S
 --assembly
     Compile to assembly text file.
 
---merge:Merge[0-] <name=file>
-    Merge additinal WebAssembly module into the output. The module can contain only functions.
-    If name is prefixed with '!', exports from module will be available as global (guest-host)
-    exports. Otherwise, the module exports can be used only internally by other modules.
+--merge:Merge[0-] <name>\\=<file>
+    Merge additinal WebAssembly module into the output. The module can contain
+    only functions. If name is prefixed with "!", exports from module will be
+    available as global (guest-host) exports. Otherwise, the module exports can
+    be used only internally by other modules.
 
 --debug-dump
-    Dump various internal files alongside the output file. Use it to debug the compiler.
+    Dump various internal files alongside the output file. Use it to debug the
+    compiler.
 
 --version:!ver
     Display version information.
 
 --help:!help
     Display this information.
-
 ```;
 
 const filters = {
