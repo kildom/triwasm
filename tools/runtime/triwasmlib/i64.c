0986448613810ae8b5605071803ee17713eb025c
kildom <kildom@users.noreply.github.com>
2023-12-26 14:19:59 +0100

    Draft of better udivmod emulation

--- tools/runtime/triwasmlib/i64.c
+++ tools/runtime/triwasmlib/i64.c
@@ -2,11 +2,17 @@
 // SPDX-License-Identifier: 0BSD
 
 #include "common.h"
+#include <wasm_simd128.h>
 
 TRIVM_INLINE_ASSEMBLY(
     "// empty\n"
     )
 uint64_t make64(uint32_t h, uint32_t l);
+
+TRIVM_INLINE_ASSEMBLY(
+    "// empty\n"
+    )
+v128 make128(uint64_t h, uint64_t l);
 
 
 TRIVM_EXPORT(sub64)
@@ -18,6 +24,7 @@ uint64_t sub64(uint32_t bh, uint32_t bl, uint32_t ah, uint32_t al)
     }
     al -= bl;
     ah -= bh + carry;
+    // TODO: when converted to triasm, add discardable block that saves second carry to GPR0
     return make64(ah, al);
 }
 
@@ -57,8 +64,111 @@ uint64_t ushr64_1(uint32_t ah, uint32_t al)
     return make64(ah, al);
 }
 
+
+TRIVM_ASSEMBLY(
+    "__triwasmlib__udivmod64_asm:\n"
+    ".LOCAL loop_start"
+    ".LOCAL skip_res"
+    ".LOCAL add_res"
+    // al, ah, bl, bh, ret, ret_latest
+    "WRITE GPR1\n"
+    // al, ah, bl, bh, ret
+    "READ [SP] - 8\n"
+    "READ [SP] - 8\n"
+    "CALL __triwasmlib__clz64\n"
+    // al, ah, bl, bh, ret, zeros
+    "READ [SP] - 12\n"
+    "READ [SP] - 12\n"
+    "READ [SP] - 8\n"
+    "CALL __triwasmlib__shl64\n"
+    // al, ah, bl, bh, ret, zeros, bl, bh
+    "WRITE [SP] - 12\n"
+    "WRITE [SP] - 12\n"
+    // al, ah, bl, bh, ret, zeros
+    "WRITE GPR0\n"
+    "NEG -1\n"
+    "NEG 0\n"
+    "READ [SP]\n"
+    "READ [SP]\n"
+    "READ GPR0\n"
+    "CALL __triwasmlib__shl64\n"
+    // al, ah, bl, bh, ret, shift:lo, shift:hi, res:lo, res:hi
+    "loop_start:\n"
+    "READ [SP] - 32\n"
+    "READ [SP] - 32\n"
+    "READ [SP] - 32\n"
+    "READ [SP] - 32\n"
+    ".REF __triwasmlib__sub64_save_carry"  //TODO: Referencing this label will enable discardable block in sub64 that saves carry to GPR0
+    "CALL __triwasmlib__sub64\n"
+    "READ GRP0\n"
+    "BRF add_res\n"
+    "WRITE GRP0\n"
+    "WRITE GRP0\n"
+    "BR skip_res\n"
+    "add_res:"
+    // al, ah, bl, bh, ret, shift:lo, shift:hi, res:lo, res:hi, al, ah
+    "WRITE [SP] - 32\n"
+    "WRITE [SP] - 32\n"
+    // al, ah, bl, bh, ret, shift:lo, shift:hi, res:lo, res:hi
+    "READ [SP] - 12\n"
+    "READ [SP] - 12\n"
+    // al, ah, bl, bh, ret, shift:lo, shift:hi, res:lo, res:hi, shift:lo, shift:hi
+    "CALL __triwasmlib__or64\n"
+    "skip_res:\n"
+    // al, ah, bl, bh, ret, shift:lo, shift:hi, res:lo, res:hi
+    "READ [SP] - 24\n"
+    "READ [SP] - 24\n"
+    "CALL __triwasmlib__ushr64_1\n"
+    "WRITE [SP] - 24\n"
+    "WRITE [SP] - 24\n"
+    "READ [SP] - 12\n"
+    "READ [SP] - 12\n"
+    "CALL __triwasmlib__ushr64_1\n"
+    "READ [SP] - 4\n"
+    "READ [SP] - 4\n"
+    // al, ah, bl, bh, ret, shift:lo, shift:hi, res:lo, res:hi, shift:lo, shift:hi, shift:lo, shift:hi
+    "WRITE [SP] - 20\n"
+    "WRITE [SP] - 20\n"
+    // al, ah, bl, bh, ret, shift:lo, shift:hi, res:lo, res:hi, shift:lo, shift:hi
+    "OR\n"
+    "BRT loop_start\n"
+    // al, ah, bl, bh, ret, shift:lo, shift:hi, res:lo, res:hi
+    "READ [SP] - 16\n"
+    // al, ah, bl, bh, ret, shift:lo, shift:hi, res:lo, res:hi, ret
+    "READ GPR1\n"
+    "WRITE PC\n"
+    )
+uint64_t udivmod64_asm(uint64_t a, uint64_t b);
+
+TRIVM_ASSEMBLY(
+    // al, ah, bl, bh, ret
+    "CALL __triwasmlib__udivmod64_asm\n"
+    // rem:lo, rem:hi, x, x, x, x, x, res:lo, res:hi, ret
+    ".if TRIVM_EXT_UNWIND\n"
+    "RUNWIND 2, 7\n"
+    ".else\n"
+    "NEG -0x26\n"
+    "BR __trivm__unwind8_ret\n"
+    ".end\n"
+    )
+uint64_t udiv64_asm(uint64_t a, uint64_t b);
+
+TRIVM_ASSEMBLY(
+    // al, ah, bl, bh, ret
+    "CALL __triwasmlib__udivmod64_asm\n"
+    // rem:lo, rem:hi, x, x, x, x, x, res:lo, res:hi, ret
+    ".if TRIVM_EXT_UNWIND\n"
+    "RUNWIND 0, 7\n"
+    ".else\n"
+    "NEG -0x06\n"
+    "BR __trivm__unwind8_ret\n"
+    ".end\n"
+    )
+uint64_t umod64_asm(uint64_t a, uint64_t b);
+
+
 TRIVM_EXPORT(udiv64)
-uint64_t udivmod64(uint32_t is_mod, uint64_t b, uint64_t a)
+v128 udivmod64(uint32_t is_mod, uint64_t b, uint64_t a)
 {
     uint32_t zeros = __builtin_clzll(b);
     b <<= zeros;
@@ -72,7 +182,7 @@ uint64_t udivmod64(uint32_t is_mod, uint64_t b, uint64_t a)
         shift >>= 1;
         b >>= 1;
     }
-    return is_mod ? a : result;
+    return make128(a, result);
 }
 
 TRIVM_EXPORT(i32_clz)
